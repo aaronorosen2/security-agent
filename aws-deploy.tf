2bc1a3e8a1ffe34ef5c43996458d0f8606e02f4e
aaronorosen@gmail.com <Aaron Rosen>
2023-09-05 22:52:33 -0700

    Remove godaddy terraform-provider due to blocking issue #45 and use aws

--- aws-deploy.tf
+++ aws-deploy.tf
@@ -3,9 +3,6 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
-    godaddy = {
-      source = "n3integration/godaddy"
-    }
   }
 }
 
@@ -102,272 +99,45 @@ resource "local_file" "hosts_cfg" {
   filename = "hosts.ini"
 }
 
-resource "godaddy_domain_record" "gd-fancy-domain" {
-  domain = "realtorstat.com"
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
 
-  nameservers = [
-    "ns27.domaincontrol.com",
-    "ns28.domaincontrol.com",
+  zones = {
+    "terraform-aws-modules-agentstat.net" = {
+      comment = "terraform-aws-modules-agentstat.net (production)"
+      tags = {
+        env = "production"
+      }
+    }
+
+    "agentstat.net" = {
+      comment = "agentstat.net"
+    }
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = keys(module.zones.route53_zone_zone_id)[0]
+
+  records = [
+    {
+      name    = "security-agent"
+      type    = "A"
+      ttl     = 3600
+      records = [
+        aws_instance.csg_security_agent[0].public_ip,
+      ]
+    },
   ]
 
-
-  record {
-    data     = "130.127.133.103"
-    name     = "@"
-    port     = 0
-    priority = 0
-    ttl      = 600
-    type     = "A"
-    weight   = 0
-  }
-  record {
-    data     = "130.127.133.103"
-    name     = "admin"
-    port     = 0
-    priority = 0
-    ttl      = 600
-    type     = "A"
-    weight   = 0
-  }
-  record {
-    data     = "130.127.133.103"
-    name     = "app"
-    port     = 0
-    priority = 0
-    ttl      = 600
-    type     = "A"
-    weight   = 0
-  }
-  record {
-    data     = "130.127.133.103"
-    name     = "v2"
-    port     = 0
-    priority = 0
-    ttl      = 600
-    type     = "A"
-    weight   = 0
-  }
-  record {
-    data     = "130.127.133.103"
-    name     = "www"
-    port     = 0
-    priority = 0
-    ttl      = 600
-    type     = "A"
-    weight   = 0
-  }
-  record {
-    data     = "3IpI2h5Mqux4SxxFgwWQQNLJnMqXn6SrW27-6uu6kzQ"
-    name     = "_acme-challenge.admin"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "5Qc1OfkhivW1r3BNkpJdtKpEOyoC2UEWVc5Z2opURA8"
-    name     = "_acme-challenge.www"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "KjWULfv6voVUlSlLXPdEJU66YWc5-p95ihmxFhiRHik"
-    name     = "_acme-challenge"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "NETORGFT14405637.onmicrosoft.com"
-    name     = "@"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "ShWk7FKbG4T-7wTVNTYlnymmIggZ15XN6mo3-VW5t2U"
-    name     = "_acme-challenge.app"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "aisqyzomd4k65wosv4rljp23a7w2ayjd.dkim.amazonses.com"
-    name     = "aisqyzomd4k65wosv4rljp23a7w2ayjd._domainkey"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "autodiscover.outlook.com"
-    name     = "autodiscover"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "clientconfig.microsoftonline-p.net"
-    name     = "msoid"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "email.secureserver.net"
-    name     = "email"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "esgvtd4xwp2awayzewfkl2zdwgmvfojl.dkim.amazonses.com"
-    name     = "esgvtd4xwp2awayzewfkl2zdwgmvfojl._domainkey"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "google-site-verification=y6TdrzsdkKTQtzqE0grBTsZN_ox0iweBVSl2uVaobE0"
-    name     = "@"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "grofjhcghtugm65u7qbaovcjgrtv2xnq.dkim.amazonses.com"
-    name     = "grofjhcghtugm65u7qbaovcjgrtv2xnq._domainkey"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "j3qa4m34qesdcqtkfe3wffmhkgujr6yc.dkim.amazonses.com"
-    name     = "j3qa4m34qesdcqtkfe3wffmhkgujr6yc._domainkey"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "kb6ppddu434e7oe6hlm6i4ks3ftrppei.dkim.amazonses.com"
-    name     = "kb6ppddu434e7oe6hlm6i4ks3ftrppei._domainkey"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "realtorstat-com.mail.protection.outlook.com"
-    name     = "@"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "MX"
-    weight   = 0
-  }
-  record {
-    data     = "rjrgepqx4ztoiyxfn3giv6mvdu5lnpqu.dkim.amazonses.com"
-    name     = "rjrgepqx4ztoiyxfn3giv6mvdu5lnpqu._domainkey"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "sipdir.online.lync.com"
-    name     = "@"
-    port     = 443
-    priority = 100
-    protocol = "_tls"
-    service  = "_sip"
-    ttl      = 3600
-    type     = "SRV"
-    weight   = 1
-  }
-  record {
-    data     = "sipdir.online.lync.com"
-    name     = "sip"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = "sipfed.online.lync.com"
-    name     = "@"
-    port     = 5061
-    priority = 100
-    protocol = "_tcp"
-    service  = "_sipfederationtls"
-    ttl      = 3600
-    type     = "SRV"
-    weight   = 1
-  }
-  record {
-    data     = "tt8sILXK_jqdCBSiBHlAZ755iUEfHC-mPnGDyjqg4JA"
-    name     = "_acme-challenge.v2"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "v=spf1 include:secureserver.net -all"
-    name     = "@"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "TXT"
-    weight   = 0
-  }
-  record {
-    data     = "webdir.online.lync.com"
-    name     = "lyncdiscover"
-    port     = 0
-    priority = 0
-    ttl      = 3600
-    type     = "CNAME"
-    weight   = 0
-  }
-  record {
-    data     = aws_instance.csg_security_agent[0].public_ip
-    name     = "security-agent"
-    port     = 0
-    priority = 0
-    ttl      = 600
-    type     = "A"
-    weight   = 0
-  }
-
-
+  depends_on = [module.zones]
 }
 
--- install.sh
+++ install.sh
@@ -7,9 +7,26 @@ terraform plan
 terraform apply -auto-approve
 
 
+dns_ip=`tail -n 1 hosts.ini   | awk '{print $1;}'`
+
+
+
+# Here we check for dns to resolve correctly before continuing on.
+# NOTE: this is still error prone and we should use public_ip instead.
+while : ; do
+  echo "Checking if dns resolves to intended ip"
+  nslookup security-agent.agentstat.net | grep $dns_ip
+  if [ $? -eq 0 ]
+  then
+    break
+  fi
+  sleep 4
+done
+
+
 # To ignore host key checking prompt
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 # retry ssh host could be still booting up.
 export ANSIBLE_SSH_RETRIES=9
-ansible-playbook -i hosts.ini -l agents csg_security_agent.yml -e "TOKEN=$TOKEN"
+ansible-playbook -i dns-hosts.ini -l agents csg_security_agent.yml -e "TOKEN=$TOKEN"

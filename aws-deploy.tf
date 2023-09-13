339daa32f9b3074b41694b27fa18b28e8ee9ff49
aaronorosen@gmail.com <Aaron Rosen>
2023-09-13 11:02:21 -0700

    handle zone creation in tf script again

--- aws-deploy.tf
+++ aws-deploy.tf
@@ -111,15 +111,32 @@ resource "local_file" "dns_hosts_ini" {
   filename = "dns-hosts.ini"
 }
 
-data "aws_route53_zone" "hosted_zone" {
-  name = var.domain_name
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
+
+  zones = {
+    "agentstat.net" = {
+		domain_name = "agentstat.net"
+    }
+  }
 }
 
-resource "aws_route53_record" "site_domain" {
+module "records" {
+ source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = keys(module.zones.route53_zone_zone_id)[0]
+
   count   = length(aws_instance.csg_security_agent)
-  zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = "${var.record_name}-${count.index + 1}"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.csg_security_agent[count.index].public_ip]
+  records = [
+    {
+		name    = "${var.record_name}-${count.index + 1}"
+      type    = "A"
+      ttl     = 3600
+		records = [aws_instance.csg_security_agent[count.index].public_ip]
+    },
+  ]
+
+  depends_on = [module.zones]
 }
